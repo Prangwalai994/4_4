07035a9108bdac97b283ea88cd254026763519a1
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 23:54:28 +0700

    4_4 Modify 2

---
+++ DemeritPointsCalculatorUnitTest4_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemeritPointsCalculatorUnitTest4_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemeritPointsCalculatorUnitTest4_4")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc540614-9dcc-4e0c-9473-8f2f724145b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DemeritPointsCalculatorUnitTest4_4/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DemeritPointsCalculatorUnitTest4_4
+{
+    [TestFixture]
+    public class DemeritPointsCalculator
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(30)]
+
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException()
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            Assert.That(()=>calculator.CalculateDemeritPoints(-1),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+   
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed)
+        {
+
+        }
+    }
+}
